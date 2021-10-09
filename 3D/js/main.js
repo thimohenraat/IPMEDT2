7ab8e8e5d894851a55ac02768c6c2cb85065d78b
thimohenraat <thimohenraat@gmail.com>
2021-10-09 11:59:19 +0200

    bloem sclae met scroll

--- 3D/js/main.js
+++ 3D/js/main.js
@@ -58,7 +58,7 @@ let bloem;
 glftLoader.load("bloem.glb", (gltf) => {
   bloem = gltf.scene;
   bloem.rotation.x = 1.5;
-  bloem.position.set(25, -50, -90);
+  bloem.position.set(25, -50, -125);
 
   scene.add(bloem);
 })
@@ -93,19 +93,6 @@ const addStar = () => {
 }
 
 Array(200).fill().forEach(addStar)
-
-
-// const beweegBloem = () => {
-//   let positie = window.scrollY;
-//   positie = positie/50;
-//   // console.log(positie;
-//   if (bloem){  
-//     bloem.position.z = 20 - positie;
-//   }
-// }
-
-// document.body.onscroll = beweegBloem;
-// beweegBloem();
 
 
 const clock = new THREE.Clock();
@@ -185,7 +172,7 @@ const scrollHandler = () => {
 
   let originX = 25;
   let originY = -50;
-  let originZ = -90;
+  let originZ = -125;
 
   const setPosition = (axis, origin, target) => {
     let distance;
@@ -201,6 +188,9 @@ const scrollHandler = () => {
   setPosition('y', originY, targetY);
   setPosition('z', originZ, targetZ);
 
+  let scale = scrollPercentRounded / 100;
+
+  bloem.scale.set(scale,scale,scale)
 }
 
 window.addEventListener('scroll', scrollHandler);
