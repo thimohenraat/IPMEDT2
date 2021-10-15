26b29e1e02f00c4736a79fd1d0a8fbd510328953
Serilu <redkirbyog@gmail.com>
2021-10-15 14:03:29 +0200

    water pagina toevoegen

---
+++ 3D/js/water.js
@@ -0,0 +1,26 @@
+const golf = document.querySelectorAll("#golf1, #golf2, #golf3, #golf4");
+const buttonTsunami = document.getElementById("tsunami");
+const buttonQuake = document.getElementById("quake");
+
+buttonTsunami.onclick = () => {
+    golf.forEach(element => {
+        element.style.height = "80vh";
+    });
+
+    setTimeout(() => {
+        window.scrollTo(0, document.body.scrollHeight);
+     }, 3000);
+};
+
+buttonQuake.onclick = () => {
+    buttonTsunami.style.animation = "shake 0.5s linear infinite alternate";
+    buttonQuake.style.animation = "shake2 0.5s linear infinite alternate";
+    
+
+    setTimeout(() => {
+        window.scrollTo(0, document.body.scrollHeight);
+        buttonTsunami.style.animation = "none";
+        buttonQuake.style.animation = "none";
+     }, 3000);
+};
+
