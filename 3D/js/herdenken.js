29d211ab4fadd102de8a69a9c82b4e00615235b1
Thimo <thimohenraat@gmail.com>
2021-10-17 19:37:48 +0200

    bloemen slepen

--- 3D/js/herdenken.js
+++ 3D/js/herdenken.js
@@ -1,17 +1,43 @@
-const fill = document.querySelectorAll(".boeket__1, .boeket__2, .boeket__3");
-const empty = document.querySelectorAll(".kindje__1, kindje__2, kindje__3");
+const boeket = document.querySelectorAll(".boeket__1, .boeket__2, .boeket__3");
+const kind = document.querySelectorAll(".kindje__1, .kindje__2, .kindje__3");
 
-const dragStart = () => {
-    element.classList.add('invisible');
-}   
+const mover = (element) => {
+    console.log(element);
+    element.onmousedown = (e) => {
+        //waar is de muis tenopzichte van het plaatje
+        let thisY = e.offsetY;
+        let thisX = e.offsetX;
+        console.log(thisY, thisX);
 
-const dragEnd = () => {
-    console.log("noo")
+        document.onmousemove = (e) => {
+            //verplaats het plaatje naar waar de muis beweegt
+            element.style.top = e.clientY - thisY + "px";
+            element.style.left = e.clientX - thisX + "px";
+            const left = element.offsetLeft;
+            const top = element.offsetTop;
+            console.log(left, top);
+            
+            //stop met bewegen als er niet meer geklikt word
+            document.onmouseup = () => {
+                document.onmousemove = () => {
+                    // niks
+                }
+            }
+
+        } 
+
+    }
+
 }
 
+boeket.forEach(element => {
+    mover(element);
 
-fill.forEach(element => {
-    element.addEventListener('dragstart', dragStart);
 });
 
-fill.addEventListener('dragend', dragEnd);
+kind.forEach(element => {
+    const left = element.offsetLeft;
+    const top = element.offsetTop;
+    console.log(left, top);
+});
+
