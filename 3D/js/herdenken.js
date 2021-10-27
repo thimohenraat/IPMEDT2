52f376d88c0e071cb1bacf4ef54f8a406356c875
Serilu <redkirbyog@gmail.com>
2021-10-27 14:18:48 +0200

    zon toegevoegd + boot

--- 3D/js/herdenken.js
+++ 3D/js/herdenken.js
@@ -2,7 +2,7 @@ const fill = document.querySelectorAll(".boeket__1, .boeket__2, .boeket__3");
 const empty = document.querySelectorAll(".kindje__1, kindje__2, kindje__3");
 
 const dragStart = () => {
-    element.classList.add('invisible');
+    element.classList.add('hold');
 }   
 
 const dragEnd = () => {
